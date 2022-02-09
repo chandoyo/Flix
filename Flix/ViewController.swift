3688bcacac6606a80bc8f2c0be6502e50d8fa5f9
claudia <chandoyo@ucsd.edu>
2022-02-08 23:35:27 -0800

    flix 2

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -68,6 +68,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Loading up the details screen")
+        
+        // Find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // pass selected movie to details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        // deselect so movie doesn't stay highlighted
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
